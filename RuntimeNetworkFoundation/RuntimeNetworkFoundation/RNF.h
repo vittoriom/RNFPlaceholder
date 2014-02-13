65ebf4df785187a5fec94df5058b9c11636054b8
Vittorio Monaco <vittorio.monaco1@gmail.com>
2014-02-13 22:14:10 +0100

    Added protocol for future operation serializer

--- RuntimeNetworkFoundation/RuntimeNetworkFoundation/RNF.h
+++ RuntimeNetworkFoundation/RuntimeNetworkFoundation/RNF.h
@@ -22,6 +22,7 @@
 #import "RNFEndpoint.h"
 #import "RNFEndpointManager.h"
 #import "RNFValueTransformer.h"
+#import "RNFOperationSerializer.h"
 #import "NSMethodSignature+OperateOnSelectors.h"
 #import "RNFTypes.h"
 
