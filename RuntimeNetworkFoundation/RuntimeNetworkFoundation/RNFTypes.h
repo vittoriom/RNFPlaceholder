178211efc5fbb808e646301b19aada38bb7fe03e
Vittorio Monaco <vittorio.monaco1@gmail.com>
2014-02-14 18:45:57 +0100

    Added some documentation

--- RuntimeNetworkFoundation/RuntimeNetworkFoundation/RNFTypes.h
+++ RuntimeNetworkFoundation/RuntimeNetworkFoundation/RNFTypes.h
@@ -11,12 +11,39 @@
 
 @protocol RNFOperation;
 
+/**
+ *  Completion block that includes three parameters
+ *
+ *  @param response   The real response from the network call
+ *  @param operation  The operation that generated this response
+ *  @param statusCode The HTTP status code
+ */
 typedef void(^RNFCompletionBlock)(id response, id<RNFOperation> operation, NSUInteger statusCode);
 
+/**
+ *  The basic completion block, for when you don't actually care a lot :)
+ *
+ *  @param response The real response from the network call
+ */
 typedef void(^RNFCompletionBlockBasic)(id response);
 
+/**
+ *  The most complete completion block
+ *
+ *  @param response   The real response from the network call
+ *  @param operation  The operation that generated this response
+ *  @param statusCode The HTTP status code
+ *  @param cached     YES if the response was cached, NO if it's fresh from the web
+ */
 typedef void(^RNFCompletionBlockComplete)(id response, id<RNFOperation> operation, NSUInteger statusCode, BOOL cached);
 
+/**
+ *  The error block called when something goes wrong
+ *
+ *  @param response   The real response (if any)
+ *  @param error      The error
+ *  @param statusCode The HTTP status code
+ */
 typedef void(^RNFErrorBlock)(id response, NSError *error, NSUInteger statusCode);
 
 #endif
