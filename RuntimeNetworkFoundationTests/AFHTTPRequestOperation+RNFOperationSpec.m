e639fd1a2da179f491250b0ea5b4d4579f9c251a
Vittorio Monaco <vittorio.monaco1@gmail.com>
2014-03-25 22:33:13 +0100

    Scaffolding AFHTTPRequestOperation tests

---
+++ RuntimeNetworkFoundationTests/AFHTTPRequestOperation+RNFOperationSpec.m
@@ -0,0 +1,38 @@
+//
+//  AFHTTPRequestOperation+RNFOperationSpec.m
+//  RuntimeNetworkFoundation
+//
+//  Created by Vittorio Monaco on 25/03/14.
+//  Copyright 2014 Vittorio Monaco. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "AFHTTPRequestOperation+RNFOperation.h"
+
+SPEC_BEGIN(AFHTTPRequestOperation_RNFOperationSpec)
+
+describe(@"AFHTTPRequestOperation+RNFOperation", ^{
+    context(@"when creating a new operation", ^{
+       
+    });
+    
+    context(@"when generating a unique identifier", ^{
+        
+    });
+    
+    context(@"when executing the operation", ^{
+        context(@"when the operation requires basic auth", ^{
+            
+        });
+        
+        context(@"when the internet is not reachable", ^{
+            
+        });
+        
+        context(@"when the internet is reachable", ^{
+            
+        });
+    });
+});
+
+SPEC_END
